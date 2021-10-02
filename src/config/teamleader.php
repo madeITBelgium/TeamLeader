1a90cf9e49deec205efd1e57cd007b7391d5e05e
Made I.T <info@madeit.be>
2021-10-02 21:51:50 +0200

    Update teamleader.php

--- src/config/teamleader.php
+++ src/config/teamleader.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'api_url'       => env('TEAMLEADER_API_URL', 'https://api.teamleader.eu'),
-    'auth_url'      => env('TEAMLEADER_AUTH_URL', 'https://app.teamleader.eu'),
+    'api_url'       => env('TEAMLEADER_API_URL', 'https://api.focus.teamleader.eu'),
+    'auth_url'      => env('TEAMLEADER_AUTH_URL', 'https://focus.teamleader.eu'),
     'client_id'     => env('TEAMLEADER_ID'),
     'client_secret' => env('TEAMLEADER_SECRET'),
     'redirect_uri'  => env('TEAMLEADER_REDIRECT'),
