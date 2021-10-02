e0425637d8286f83ac9c2340e173ca44c86183ad
Made I.T <info@madeit.be>
2021-10-02 22:06:41 +0200

    BusinessType.php

---
+++ src/Crm/BusinessType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MadeITBelgium\TeamLeader\Crm;
+
+/**
+ * TeamLeader Laravel PHP SDK.
+ *
+ * @version    1.3.0
+ *
+ * @copyright  Copyright (c) 2018 Made I.T. (https://www.madeit.be)
+ * @author     Tjebbe Lievens <tjebbe.lievens@madeit.be>
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-3.txt    LGPL
+ */
+class BusinessType
+{
+    private $teamleader;
+
+    public function __construct($teamleader)
+    {
+        $this->setTeamleader($teamleader);
+    }
+
+    /**
+     * set Teamleader.
+     *
+     * @param $teamleader
+     */
+    public function setTeamleader($teamleader)
+    {
+        $this->teamleader = $teamleader;
+    }
+
+    /**
+     * get Teamleader.
+     *
+     * @param $teamleader
+     */
+    public function getTeamleader()
+    {
+        return $this->teamleader;
+    }
+
+    /**
+     * Get a list of business types.
+     */
+    public function list($data = [])
+    {
+        return $this->teamleader->getCall('businessTypes.list?'.http_build_query($data));
+    }
+}
