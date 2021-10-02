594e2a4208e23c3b19bb805cf6e8d29ecaff0bcf
Made I.T <info@madeit.be>
2021-10-02 22:09:43 +0200

    Create Address.php

---
+++ src/Crm/Address.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MadeITBelgium\TeamLeader\Crm;
+
+/**
+ * TeamLeader Laravel PHP SDK.
+ *
+ * @version    1.3.0
+ *
+ * @copyright  Copyright (c) 2018 Made I.T. (https://www.madeit.be)
+ * @author     Tjebbe Lievens <tjebbe.lievens@madeit.be>
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-3.txt    LGPL
+ */
+class Address
+{
+    private $teamleader;
+
+    public function __construct($teamleader)
+    {
+        $this->setTeamleader($teamleader);
+    }
+
+    /**
+     * set Teamleader.
+     *
+     * @param $teamleader
+     */
+    public function setTeamleader($teamleader)
+    {
+        $this->teamleader = $teamleader;
+    }
+
+    /**
+     * get Teamleader.
+     *
+     * @param $teamleader
+     */
+    public function getTeamleader()
+    {
+        return $this->teamleader;
+    }
+
+    /**
+     * Get a list of companies.
+     */
+    public function list($data = [])
+    {
+        return $this->teamleader->getCall('levelTwoAreas.list?'.http_build_query($data));
+    }
+}
