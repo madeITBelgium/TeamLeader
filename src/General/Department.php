94744594273aa46b04dbd4891e263b0050ba5038
gldrenthe89 <gl@drenthe.it>
2021-10-28 14:13:03 +0200

    Added group General with the underlying Entities -> Department -> Team -> User -> Custom Field -> Work Type

---
+++ src/General/Department.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MadeITBelgium\TeamLeader\General;
+
+/**
+ * TeamLeader Laravel PHP SDK.
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) 2018 Invato B.V. (https://invato.nl)
+ * @author     Geert Lucas Drenthe <gl@drenthe.it>
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-3.txt    LGPL
+ */
+class Department
+{
+    private $teamleader;
+
+    public function __construct($teamleader)
+    {
+        $this->setTeamleader($teamleader);
+    }
+
+    /**
+     * set Teamleader.
+     *
+     * @param $teamleader
+     */
+    public function setTeamleader($teamleader)
+    {
+        $this->teamleader = $teamleader;
+    }
+
+    /**
+     * get Teamleader.
+     */
+    public function getTeamleader()
+    {
+        return $this->teamleader;
+    }
+
+    /**
+     * Get a list of departments.
+     */
+    public function list($data = [])
+    {
+        return $this->teamleader->getCall('departments.list?'.http_build_query($data));
+    }
+
+    /**
+     * Get details for a single department.
+     */
+    public function info($id)
+    {
+        return $this->teamleader->getCall('departments.info?'.http_build_query(['id' => $id]));
+    }
+}
