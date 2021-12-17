18a3e145c30b9e03e240bf2544082749920eb187
Made I.T <info@madeit.be>
2021-12-17 14:20:25 +0100

    Create TaxRates.php

---
+++ src/Invoicing/TaxRates.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MadeITBelgium\TeamLeader\Invoicing;
+
+/**
+ * TeamLeader Laravel PHP SDK.
+ *
+ * @version    1.5.0
+ *
+ * @copyright  Copyright (c) 2018 Made I.T. (https://www.madeit.be)
+ * @author     Tjebbe Lievens <tjebbe.lievens@madeit.be>
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-3.txt    LGPL
+ */
+class TaxRates
+{
+    private $teamleader;
+
+    public function __construct($teamleader)
+    {
+        $this->setTeamleader($teamleader);
+    }
+
+    /**
+     * set Teamleader.
+     *
+     * @param $teamleader
+     */
+    public function setTeamleader($teamleader)
+    {
+        $this->teamleader = $teamleader;
+    }
+
+    /**
+     * get Teamleader.
+     *
+     * @param $teamleader
+     */
+    public function getTeamleader()
+    {
+        return $this->teamleader;
+    }
+
+    /**
+     * Get a list of available tax rates.
+     */
+    public function list($data = [])
+    {
+        return $this->teamleader->getCall('taxRates.list?'.http_build_query($data));
+    }
+}
